4921ecac6eaa52d2439a53261fc3e2a78e733c0d
jackdw258 <64153468+jackdw258@users.noreply.github.com>
2024-12-14 10:14:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Rate My Avatar", "Rate My Avatar"];
+const statusMessages = ["For Sale", "Make a Ticket!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
